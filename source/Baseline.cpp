7914063de2032dcd3b5d98c6e4c4fc0ec4cff181
karis <yseah009@e.ntu.edu.sg>
2018-06-11 16:04:11 +0800

    fit matrix

--- source/Baseline.cpp
+++ source/Baseline.cpp
@@ -10,88 +10,219 @@
 #include <iomanip>
 #include "powerful.h"
 
+#include<string>
+#include<sstream>
+
+
 using namespace std;
 using namespace NTL;
 using namespace chrono;
 
 typedef high_resolution_clock Clock;
 
-int main()
-{
-	cout << "Hello, World!" << endl;
+//ZZX msg;
+//void encode(int x);
 
-	cout << "-------------------- Initialization --------------------" << endl;
-	auto begin_init = Clock::now();
+int main() {
+    cout << "Hello, World!" << endl;
 
-	long m = 8;                                         // Specific modulus
-	long p = 17;                                        // Plaintext base
-	long r = 1;                                         // Lifting
-	long L = 5;                                         // Number of levels in the modulus chain
-	long c = 1;                                         // Number of columns in key-switching matrix
-	long w = 2;                                         // Hamming weight of secret key
-	long d = 1;                                         // Degree of the field extension
-	long k = 80;                                        // Security parameter
-	long s = 0;                                         // Minimum number of slots
+    cout << "-------------------- Initialization --------------------" << endl;
+    auto begin_init = Clock::now();
 
-	// Finding m
-//	m = FindM(k, L, c, p, d, s, 0);                     // Find a value for m given the params
-	cout << "m = " << m << endl;
+    long m = 8;                                         // Specific modulus
+    long p = 17;                                        // Plaintext base
+    long r = 1;                                         // Lifting
+    long L = 5;                                         // Number of levels in the modulus chain
+    long c = 1;                                         // Number of columns in key-switching matrix
+    long w = 2;                                         // Hamming weight of secret key
+    long d = 1;                                         // Degree of the field extension
+    long k = 80;                                        // Security parameter
+    long s = 0;                                         // Minimum number of slots
 
-	// Initializing context
-	FHEcontext context(m, p, r);                        // Initialize context
-	buildModChain(context, L, c);                       // Modify the context, adding primes to the modulus chain
+    // Finding m
+	//m = FindM(k, L, c, p, d, s, 0);                     // Find a value for m given the params
+    cout << "m = " << m << endl;
 
-	// Creating polynomial
-	ZZX G = context.alMod.getFactorsOverZZ()[0];       // Creates the polynomial for a plaintext slot
-	cout << "degree: " << deg(G) << endl;
-	cout << "G: " << G << endl;
+    // Initializing context
+    FHEcontext context(m, p, r);                        // Initialize context
+    buildModChain(context, L, c);                       // Modify the context, adding primes to the modulus chain
 
-	// Generating keys
-	FHESecKey secretKey(context);                       // Construct a secret key structure
-	FHEPubKey &publicKey = secretKey;                   // An "upcast": FHESecKey is a subclass of FHEPubKey - Creates publicKey from secretKey
-	secretKey.GenSecKey(w);                             // Generate a secret key with Hamming weight w
-	addSome1DMatrices(secretKey);                       // apparently for key switching
-	addFrbMatrices(secretKey);                          // for Ctxt rotate
+    // Creating polynomial
+    ZZX G = context.alMod.getFactorsOverZZ()[0];       // Creates the polynomial for a plaintext slot
+    cout << "degree: " << deg(G) << endl;
+    cout << "G: " << G << endl;
 
-	// Helper Class for encryption and decryption
-	EncryptedArray ea(context, G);                      // Construct EncryptedArray object ea, associated with context and G
-	long nslots = ea.size();                            // Number of slots in the plaintext encoding
-	cout << "slots: " << nslots << endl;
+    // Generating keys
+    FHESecKey secretKey(context);                       // Construct a secret key structure
+    FHEPubKey &publicKey = secretKey;                   // An "upcast": FHESecKey is a subclass of FHEPubKey - Creates publicKey from secretKey
+    secretKey.GenSecKey(w);                             // Generate a secret key with Hamming weight w
+//	addSome1DMatrices(secretKey);                       // apparently for key switching
+//	addFrbMatrices(secretKey);                          // for Ctxt rotate
 
-	auto end_init = Clock::now();
-	cout << "FHE Ready!" << endl;
-	cout << "It took: " << duration_cast<seconds>(end_init - begin_init).count() << " seconds." << endl;
+    // Helper Class for encryption and decryption
+    EncryptedArray ea(context, G);                      // Construct EncryptedArray object ea, associated with context and G
+    long nslots = ea.size();                            // Number of slots in the plaintext encoding
+    cout << "slots: " << nslots << endl;
 
-
-	cout << "-------------------- Encryption --------------------" << endl;
-	auto begin_encrypt = Clock::now();
-	zz_pX poly_p;
-	SetCoeff(poly_p, 1);
-	ZZX ptxt_poly = conv<ZZX>(poly_p);
-	PolyRed(ptxt_poly, 17, true); // reduce to the symmetric interval
-
-	cout << "ptxt_poly: " << ptxt_poly << endl;
-
-	Ctxt enc(publicKey);
-	publicKey.Encrypt(enc, ptxt_poly);
-
-	auto end_encrypt = Clock::now();
-	cout << "Encryption Over!" << endl;
-	cout << "It took: " << duration_cast<seconds>(end_encrypt - begin_encrypt).count() << " seconds." << endl;
+    auto end_init = Clock::now();
+    cout << "FHE Ready!" << endl;
+    cout << "It took: " << duration_cast<seconds>(end_init - begin_init).count() << " seconds." << endl;
 
 
-	cout << "-------------------- Operation --------------------" << endl;
-	Ctxt temp = enc;
-	for (int i=0 ; i<4 ; i++)
-	{
-		enc.multiplyBy(temp);
-	}
 
 
-	cout << "-------------------- Decryption --------------------" << endl;
-	ZZX ans;
-	secretKey.Decrypt(ans, enc);
-	cout << "Plaintext:  " << ans << endl;
 
-	return 0;
+
+    cout << endl;
+    int x, y;
+
+    ifstream myfile;
+    myfile.open("/home/karis/CLionProjects/HElib-basic/matrix.txt");
+
+    // Tests if the file opens successfully.
+    if (!myfile.is_open()) {
+        cout << "File failed to open!" << endl;
+    }
+
+    int A[x][y];
+
+//    myfile >> a >> b;
+//    cout << a << endl;
+//    cout << b << endl;
+
+//    string line;
+
+//    getline(myfile, x);
+//    getline(myfile, y);
+
+    while (getline(myfile, line)) {
+        cout << line << endl;
+    }
+
+
+
+
+
+
+//    for (int i = 0; i < x; i++) {
+//        myfile >> A[i][j];
+//    }
+//    cout << A << endl;
+
+/*    cout << endl;
+    // Initialize digits to be 0 and let dividend = x.
+    int x = 14, remainder, digits = 0, dividend = x;
+
+    // Stops when the dividend becomes 0.
+    while (dividend != 0) {
+        dividend = dividend / 2;
+        digits++;
+    }
+
+    int arr[digits];
+
+    // Initialize dividend to be x again so that it doesnt use the updated dividend values.
+    dividend = x;
+
+    // Array placement starts from 0. First placement is the constant's binary.
+    for (int i = 0; i < digits; i++) {
+        remainder = dividend % 2;
+        arr[i] = remainder;
+        dividend = dividend / 2;
+    }
+
+    ZZX msg;
+    // Prints out the array of binary numbers.
+    for (int i = 0; i < digits; i++) {
+        if (arr[i] == 1) {
+            SetCoeff(msg, i);
+        }
+    }
+*/
+
+
+/*    cout << "-------------------- Encryption --------------------" << endl;
+    auto begin_encrypt = Clock::now();
+
+    //int x = 5;
+    //encode(x);
+
+    //ZZX msg;
+    //SetCoeff(msg,1,1);
+    //SetCoeff(msg,2,1); // 4 = x^2
+
+    cout << msg << endl;
+
+    //ZZX msg2;
+    //SetCoeff(msg2,3,1); // 8 = 1000 = x^3
+
+    Ctxt enc(publicKey), enc2(publicKey);
+    publicKey.Encrypt(enc, msg);
+    //publicKey.Encrypt(enc2, msg2);
+
+    auto end_encrypt = Clock::now();
+    cout << "Encryption Over!" << endl;
+    cout << "It took: " << duration_cast<seconds>(end_encrypt - begin_encrypt).count() << " seconds." << endl;
+
+
+    cout << "-------------------- Operation --------------------" << endl;
+    cout << "Ciphertext before operations:" << endl;
+    cout << enc << endl;
+
+    cout << "Ciphertext after addition:" << endl;
+    enc.addCtxt(enc); // expect to get 4
+    cout << enc << endl;
+
+//    cout << "Ciphertext after multiplication:" << endl;
+//    enc.multiplyBy(enc); // expect to get 16
+//    cout << enc << endl;
+
+    cout << "-------------------- Decryption --------------------" << endl;
+    ZZX ans;
+    secretKey.Decrypt(ans, enc);
+    cout << "Plaintext:  " << ans << endl;
+
+    return 0;
+*/
 }
+
+
+
+/*
+void encode(int x) {
+
+    int remainder, digits = 0, dividend = x;
+
+    // Stops when the dividend becomes 0.
+    while (dividend != 0) {
+        dividend = dividend / 2;
+        digits++;
+    }
+
+    int arr[digits];
+
+    // Initialize dividend to be x again so that it doesnt use the updated dividend values.
+    dividend = x;
+
+    // Array placement starts from 0. First placement is the constant's binary.
+    for (int i = 0; i < digits; i++) {
+        remainder = dividend % 2;
+        arr[i] = remainder;
+        dividend = dividend / 2;
+    }
+
+
+    //ZZX msg;
+
+    // Prints out the array of binary numbers.
+    for (int i = 0; i < digits; i++) {
+        if (arr[i] == 1) {
+            return SetCoeff(msg, i);
+        }
+    }
+
+
+
+
+}
+*/
