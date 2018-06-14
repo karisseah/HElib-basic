07ba896d42515170c096cebb0ad2602efa210acc
karis <yseah009@e.ntu.edu.sg>
2018-06-14 16:42:25 +0800

    dec matrix

---
+++ source/data.cpp
@@ -0,0 +1,5 @@
+//
+// Created by karis on 6/12/18.
+//
+
+#include "data.h"
---
+++ source/data.h
@@ -0,0 +1,8 @@
+//
+// Created by karis on 6/12/18.
+//
+
+#ifndef HELIB_BASIC_DATA_H
+#define HELIB_BASIC_DATA_H
+
+#endif //HELIB_BASIC_DATA_H
